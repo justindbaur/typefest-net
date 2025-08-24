4c2d09d5b8ab89b39627e3a1f59da7fe18d65999
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2025-08-24 21:17:20 +0000

    Format

--- src/TypeFest.Net.SourceGenerator/HierarchyInfo.cs
+++ src/TypeFest.Net.SourceGenerator/HierarchyInfo.cs
@@ -9,38 +9,38 @@ internal sealed record TypeInfo(string QualifiedName, TypeKind TypeKind, bool Is
 
 internal sealed record HierarchyInfo(string FilenameHint, string MetadataName, string Namespace, ImmutableEquatableArray<TypeInfo> Hierarchy)
 {
-	public static HierarchyInfo From(INamedTypeSymbol typeSymbol)
-	{
-	    var hierarchy = new List<TypeInfo>();
+    public static HierarchyInfo From(INamedTypeSymbol typeSymbol)
+    {
+        var hierarchy = new List<TypeInfo>();
 
-		for (INamedTypeSymbol? parent = typeSymbol;
-		     parent is not null;
-			 parent = parent.ContainingType)
+        for (INamedTypeSymbol? parent = typeSymbol;
+             parent is not null;
+             parent = parent.ContainingType)
         {
-			hierarchy.Add(new TypeInfo(
-				parent.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat),
-				parent.TypeKind,
-				parent.IsRecord
-			));
-		}
+            hierarchy.Add(new TypeInfo(
+                parent.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat),
+                parent.TypeKind,
+                parent.IsRecord
+            ));
+        }
 
-		return new(
-			typeSymbol.GetFullyQualifiedMetadataName(),
-			typeSymbol.MetadataName,
-			typeSymbol.ContainingNamespace.ToDisplayString(new(typeQualificationStyle: NameAndContainingTypesAndNamespaces)),
-			hierarchy.ToImmutableEquatableArray() 
-		);
-	}
+        return new(
+            typeSymbol.GetFullyQualifiedMetadataName(),
+            typeSymbol.MetadataName,
+            typeSymbol.ContainingNamespace.ToDisplayString(new(typeQualificationStyle: NameAndContainingTypesAndNamespaces)),
+            hierarchy.ToImmutableEquatableArray()
+        );
+    }
 
-	public string GlobalName()
-	{
-		var names = string.Join(".", Hierarchy.Select(t => t.QualifiedName).Reverse());
+    public string GlobalName()
+    {
+        var names = string.Join(".", Hierarchy.Select(t => t.QualifiedName).Reverse());
 
-   		if (Namespace is "")
-		{
-			return $"global::{names}";
-		}
+        if (Namespace is "")
+        {
+            return $"global::{names}";
+        }
 
-		return $"global::{Namespace}.{names}";
-	}
+        return $"global::{Namespace}.{names}";
+    }
 }
